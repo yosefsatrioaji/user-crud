c1a99c20db9df820eb17252311cda0d8b976924a
Yosef Satrio Aji <yosef260501@gmail.com>
2022-05-03 01:09:13 +0700

    preparing for docker

--- index.js
+++ index.js
@@ -27,6 +27,6 @@ app.get('/', (req, res) => {
     res.send('Hello World');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log('Server is running on port 3000');
 });
